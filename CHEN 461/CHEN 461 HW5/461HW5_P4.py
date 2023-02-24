0e878ee0f12f28342d2c3832504257a6ef20ebb2
mkl <nibhscripta@tutanota.com>
2023-02-23 19:49:07 -0800

    finish HW5

--- CHEN 461/CHEN 461 HW5/461HW5_P4.py
+++ CHEN 461/CHEN 461 HW5/461HW5_P4.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 
+# make scipy.signal look like matlab control systems toolbox
+ss = signal.lti
+step = signal.step
+impulse = signal.impulse
+tf = signal.TransferFunction
+
 # constants
 F_V = 0.5
 k_1 = 3
@@ -21,12 +27,12 @@ C = np.array([0, 1])
 D = 0
 
 # define state space system
-sys = signal.StateSpace(A, B, C, D)
+sys = ss(A, B, C, D)
 
 # compute step/impulse response
-t_step, y_step = sys.step()
+t_step, y_step = step(sys)
 
-t_impulse, y_impulse = sys.impulse()
+t_impulse, y_impulse = impulse(sys)
 
 # plot
 plt.plot(t_step, y_step, label="Step response")
@@ -38,17 +44,17 @@ plt.legend()
 plt.show()
 
 # compute transfer function
-transfer_func = sys.to_tf()
+transfer_func = tf(sys)
 
 print(transfer_func)
 
 # manually create system from transfer function
-sys_manual = signal.lti(transfer_func.num, transfer_func.den)
+sys_manual = ss(transfer_func.num, transfer_func.den)
 
 # comput step/impulse response
-t_step, y_step = sys_manual.step()
+t_step, y_step = step(sys_manual)
 
-t_impulse, y_impulse = sys_manual.impulse()
+t_impulse, y_impulse = impulse(sys_manual)
 
 plt.plot(t_step, y_step, label="Step response")
 plt.plot(t_impulse, y_impulse, label="Impulse response")
