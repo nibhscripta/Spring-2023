c4a4633f8953a0767fad976fcb19b13ba3011f32
mkl <nibhscripta@tutanota.com>
2023-03-24 23:10:17 -0700

    Finish HW8

--- CHEN 461/CHEN 461 HW8/p4.py
+++ CHEN 461/CHEN 461 HW8/p4.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control
 
+# parameters
 T = 10
-t = 0.1
+tau = 0.1
 k = 1
 
-sys = k * control.tf([-T, 1], [t, 1, 0])
+# bode
+sys = k * control.tf([-T, 1], [tau, 1, 0])
 w = np.linspace(1e-3, 1e3, int(1e5))
 mag, phase, omega = control.bode_plot(sys, omega=w)
 plt.show()
